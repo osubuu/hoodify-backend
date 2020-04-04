ee67b5056b03b6411beec4ae90af66199f62ba00
Khoi <hakhoipham@gmail.com>
2020-04-04 19:13:19 -0400

    update graphql-yoga schema + resolvers to get all items and create an item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+// Mutations in here must match mutations defined in schema.graphql
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const data = { data: { ...args } };
+    const item = await ctx.db.mutation.createItem(data, info);
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+// use forwardto if no custom logic (i.e just plain get or push data)
+
+// Queries in here must match mutations defined in schema.graphql
+const Query = {
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
