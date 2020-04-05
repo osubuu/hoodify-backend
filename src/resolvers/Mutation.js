602e4741ee68d75aeef2fef90c421333ad2e2868
Khoi <hakhoipham@gmail.com>
2020-04-05 17:17:36 -0400

    added the deleteItem mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 // Mutations in here must match mutations defined in schema.graphql
+// info refers to what we want back in our response
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -15,6 +16,14 @@ const Mutations = {
     };
     const item = await ctx.db.mutation.updateItem(params, info);
     return item;
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. Find item
+    const item = await ctx.db.query.item({ where }, '{ id title }');
+    // 2. TODO: check if they own that item or have the permissions
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);;
   }
 };
 
