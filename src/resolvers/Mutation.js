14eef50422155db17d04a8a93139940e2439e82e
Khoi <hakhoipham@gmail.com>
2020-04-05 16:04:14 -0400

    added updateItem mutation resolver + renamed variable for better clarity

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,8 +2,18 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
-    const data = { data: { ...args } };
-    const item = await ctx.db.mutation.createItem(data, info);
+    const params = { data: { ...args } };
+    const item = await ctx.db.mutation.createItem(params, info);
+    return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id; // remove id from the updates because not updating ID
+    const params = {
+      data: updates,
+      where: args.id
+    };
+    const item = await ctx.db.mutation.updateItem(params, info);
     return item;
   }
 };
