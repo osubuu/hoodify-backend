000576f5966f4cec4bfed11973c4c03a60889e2d
Khoi <hakhoipham@gmail.com>
2020-04-10 17:02:41 -0400

    add query to get currently signed in User data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
+
 const createServer = require('./createServer');
 const db = require('./db');
-const cookieParser = require('cookie-parser');
 
 const server = createServer();
 
 // Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
+
+// decode the JWT so we can get the user ID on each request
+server.express.use((request, response, next) => {
+  const { token } = request.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the request for future requests to access
+    request.userId = userId;
+  }
+  next();
+});
 
 // TODO: Use express middleware to populate current user
 
