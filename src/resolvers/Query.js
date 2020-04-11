cf2c38a730204dc5ace6c2b4f176703450962ebe
Khoi <hakhoipham@gmail.com>
2020-04-11 11:46:11 -0400

    added query to get all users

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,5 @@
-const { forwardTo } = require('prisma-binding');
-// use forwardto if no custom logic (i.e just plain get or push data)
+const { forwardTo } = require('prisma-binding'); // use forwardto if no custom logic (i.e just plain get or push data)
+const { hasPermission } = require('../utils');
 
 // Queries in here must match mutations defined in schema.graphql
 const Query = {
@@ -14,6 +14,16 @@ const Query = {
     return ctx.db.query.user({
       where: { id: ctx.request.userId }
     }, info);
+  },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 3. If they do, query all the users
+    return ctx.db.query.users({}, info);
   }
 };
 
