253f568a2399c89340cdd21c7b69583c0fd1d7bc
Khoi <hakhoipham@gmail.com>
2020-04-12 15:59:01 -0400

    added query to get a single order

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -24,6 +24,24 @@ const Query = {
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
     // 3. If they do, query all the users
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error("You don't have permission")
+    }
+    // 4. Return the order
+    return order;
   }
 };
 
